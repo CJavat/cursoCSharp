6e810e82216256d934b0694d1170ee38cb53b0ec
JavatoX <cdpm98@hotmail.com>
2022-11-04 14:09:35 -0600

    avance del curso

--- cursoDeNet/practicaDePOOGESTIONARCLIENTES.Designer.cs
+++ cursoDeNet/practicaDePOOGESTIONARCLIENTES.Designer.cs
@@ -33,6 +33,12 @@
             this.lblNombre = new System.Windows.Forms.Label();
             this.txbNombre = new System.Windows.Forms.TextBox();
             this.btnEliminar = new System.Windows.Forms.Button();
+            this.txbApellido = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txbTelefono = new System.Windows.Forms.TextBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.txbTarjetaDeCredito = new System.Windows.Forms.TextBox();
+            this.lblTarjetaDeCredito = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lstClientes
@@ -48,7 +54,7 @@
             // btnAgregar
             // 
             this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAgregar.Location = new System.Drawing.Point(299, 71);
+            this.btnAgregar.Location = new System.Drawing.Point(294, 368);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(149, 48);
             this.btnAgregar.TabIndex = 1;
@@ -69,7 +75,7 @@
             // txbNombre
             // 
             this.txbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbNombre.Location = new System.Drawing.Point(410, 12);
+            this.txbNombre.Location = new System.Drawing.Point(449, 12);
             this.txbNombre.Name = "txbNombre";
             this.txbNombre.Size = new System.Drawing.Size(252, 31);
             this.txbNombre.TabIndex = 3;
@@ -77,7 +83,7 @@
             // btnEliminar
             // 
             this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(299, 151);
+            this.btnEliminar.Location = new System.Drawing.Point(449, 368);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(149, 48);
             this.btnEliminar.TabIndex = 4;
@@ -85,11 +91,71 @@
             this.btnEliminar.UseVisualStyleBackColor = true;
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
+            // txbApellido
+            // 
+            this.txbApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbApellido.Location = new System.Drawing.Point(449, 55);
+            this.txbApellido.Name = "txbApellido";
+            this.txbApellido.Size = new System.Drawing.Size(252, 31);
+            this.txbApellido.TabIndex = 6;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(294, 58);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(120, 25);
+            this.lblApellido.TabIndex = 5;
+            this.lblApellido.Text = "APELLIDO:";
+            // 
+            // txbTelefono
+            // 
+            this.txbTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTelefono.Location = new System.Drawing.Point(449, 105);
+            this.txbTelefono.Name = "txbTelefono";
+            this.txbTelefono.Size = new System.Drawing.Size(252, 31);
+            this.txbTelefono.TabIndex = 8;
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefono.Location = new System.Drawing.Point(294, 108);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(131, 25);
+            this.lblTelefono.TabIndex = 7;
+            this.lblTelefono.Text = "TELEFONO:";
+            // 
+            // txbTarjetaDeCredito
+            // 
+            this.txbTarjetaDeCredito.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTarjetaDeCredito.Location = new System.Drawing.Point(299, 183);
+            this.txbTarjetaDeCredito.Name = "txbTarjetaDeCredito";
+            this.txbTarjetaDeCredito.Size = new System.Drawing.Size(402, 31);
+            this.txbTarjetaDeCredito.TabIndex = 10;
+            // 
+            // lblTarjetaDeCredito
+            // 
+            this.lblTarjetaDeCredito.AutoSize = true;
+            this.lblTarjetaDeCredito.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTarjetaDeCredito.Location = new System.Drawing.Point(294, 155);
+            this.lblTarjetaDeCredito.Name = "lblTarjetaDeCredito";
+            this.lblTarjetaDeCredito.Size = new System.Drawing.Size(246, 25);
+            this.lblTarjetaDeCredito.TabIndex = 9;
+            this.lblTarjetaDeCredito.Text = "TARJETA DE CREDITO:";
+            // 
             // practicaDePOOGESTIONARCLIENTES
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 450);
+            this.ClientSize = new System.Drawing.Size(750, 450);
+            this.Controls.Add(this.txbTarjetaDeCredito);
+            this.Controls.Add(this.lblTarjetaDeCredito);
+            this.Controls.Add(this.txbTelefono);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.txbApellido);
+            this.Controls.Add(this.lblApellido);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.txbNombre);
             this.Controls.Add(this.lblNombre);
@@ -110,5 +176,11 @@
         private System.Windows.Forms.Label lblNombre;
         private System.Windows.Forms.TextBox txbNombre;
         private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txbApellido;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txbTelefono;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.TextBox txbTarjetaDeCredito;
+        private System.Windows.Forms.Label lblTarjetaDeCredito;
     }
 }
